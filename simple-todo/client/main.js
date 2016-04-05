1f5cc8a133fa5e21149b4beaaa25c2c2dc953ed2
John Fox <J.Arcoudis@gmail.com>
2016-04-05 18:11:52 +1000

    Refactoring and file organisation

--- simple-todo/client/main.js
+++ simple-todo/client/main.js
@@ -1,14 +1,1 @@
-import { Template } from 'meteor/templating';
-
-import './body.html';
-
-
-
-Template.body.helpers({
-    tasks:[
-        {text: "Task 1"},
-        {text: "Task 2"},
-        {text: "Task 3"},
-        {text: "Task 4"},
-    ],
-});
+import './body.js';
---
+++ simple-todo/imports/api/tasks.js
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/Mongo';
+
+export const Tasks = new Mongo.Collection('tasks');
